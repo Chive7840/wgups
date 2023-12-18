c6d39ffd8a5d63db3b99885769c20a23a86162a8
Tyler <gcarte9@wgu.edu>
2023-12-17 17:09:16 -0800

    added testing directory completed test case for graph created files for routing and hashtable

--- data_services/graph.py
+++ data_services/graph.py
@@ -20,25 +20,3 @@ class DHGraph(Generic[hub]):
 
     def get_distance(self, hub_a: hub, hub_b: hub) -> float:
         return self.edges.get(hub_a).get(hub_b)
-
-    # TODO: Fix or remove
-    # def search(self, curr_dh, destination_dh):  # Returns the distance for the provided delivery hubs
-    #     return [h for i, h in self.route_weights.items() if i[0] == curr_dh and i[1] == destination_dh]
-    #
-    # def get_dh_by_index(self, index_val):
-    #     return self.list_of_hubs[index_val]
-    #
-    # def get_dh_addr_by_id(self, dh_uid):
-    #     for uid in range(len(self.list_of_hubs)):
-    #         if self.list_of_hubs[uid].dh_id == dh_uid:
-    #             return self.list_of_hubs[uid].dh_addr
-    #
-    # def get_dh_id_by_addr(self, address):
-    #     for addr in range(len(self.list_of_hubs)):
-    #         if self.list_of_hubs[addr].dh_addr == address:
-    #             return self.list_of_hubs[addr].dh_id
-    #         else:
-    #             print("Invalid Address")
-
-
-dh_graph = DHGraph()
---
+++ test_services/test_graph.py
@@ -0,0 +1,20 @@
+from data_services.graph import DHGraph
+import unittest
+
+
+class TestDHGraph(unittest.TestCase):
+    def test_get_distance(self):
+        tst_graph = DHGraph[str]()
+        dh_a = 'Delivery Hub a'
+        dh_b = 'Delivery Hub b'
+        tst_graph.insert_hub(dh_a)
+        tst_graph.insert_hub(dh_b)
+        tst_graph.insert_edge(dh_a, dh_b, 1.0)
+        self.assertEqual(tst_graph.get_distance(dh_a, dh_b), 1.0)
+
+    # Test results
+    # with arguments python -m unittest
+    #
+    # Ran 1 test in 0.002s
+    #
+    # OK
